3ab92d262841403e0f95c993f18682a545b15331
khalilou88 <khalil.lagrida@gmail.com>
2025-08-28 15:34:50 +0200

    wip

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -1,6 +1,6 @@
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || 'development', __dirname)
 
 module.exports = defineConfig({
   projectConfig: {
